d5a4c8f5c9b772e109506248c7a7ba3a397c68ef
e <ooooooyf@163.com>
2019-06-13 10:56:39 +0800

    remove mvnw.cmd

---
+++ src/main/java/cn/gsk/mptd/MessagePushToDingApplication.java
@@ -0,0 +1,13 @@
+package cn.gsk.mptd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagePushToDingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagePushToDingApplication.class, args);
+	}
+
+}
